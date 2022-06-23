5318963505b6fcdef1fdbc53df22a698bde97d7b
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-06-23 11:26:46 -0300

    Adicionada estrutura POST funcionario

---
+++ REST-API/ViewModels/CreateFuncionarioViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restful_API.ViewModels
+{
+    public class CreateFuncionarioViewModel
+    {
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public double Salario { get; set; }
+    }
+}
