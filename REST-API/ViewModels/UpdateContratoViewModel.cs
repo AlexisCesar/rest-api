559cdc56af45eba50dff25a777ad196c3942cdac
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-06-24 10:23:39 -0300

    Estrutura refeita para comportar contratos

---
+++ REST-API/ViewModels/UpdateContratoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Restful_API.ViewModels.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restful_API.ViewModels
+{
+    public class UpdateContratoViewModel
+    {
+        [Required]
+        public string? Cargo { get; set; }
+        [Required]
+        public double Salario { get; set; }
+        [Required]
+        public TipoContratoEnum TipoContrato { get; set; }
+    }
+}
