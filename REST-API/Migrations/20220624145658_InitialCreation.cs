e425cd5c5d3455d421fc932b8c0301337b214df0
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-06-24 10:23:39 -0300

    Estrutura refeita para comportar contratos

---
+++ REST-API/Migrations/20220624145658_InitialCreation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restful_API.Migrations
+{
+    public partial class InitialCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contrato",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Inicio = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Termino = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Salario = table.Column<double>(type: "REAL", nullable: false),
+                    Cargo = table.Column<string>(type: "TEXT", nullable: true),
+                    FuncionarioId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Discriminator = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contrato", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contrato_Funcionarios_FuncionarioId",
+                        column: x => x.FuncionarioId,
+                        principalTable: "Funcionarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contrato_FuncionarioId",
+                table: "Contrato",
+                column: "FuncionarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contrato");
+
+            migrationBuilder.DropTable(
+                name: "Funcionarios");
+        }
+    }
+}
