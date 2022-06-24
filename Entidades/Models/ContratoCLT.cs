c1559a1bc36cb11abc3c244865c9575065019825
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-06-24 10:23:39 -0300

    Estrutura refeita para comportar contratos

---
+++ Entidades/Models/ContratoCLT.cs
@@ -0,0 +1,14 @@
+﻿namespace Entidades.Models
+{
+    public class ContratoCLT : Contrato
+    {
+        public ContratoCLT() : base()
+        {
+
+        }
+        public override double calcularSalario()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- REST-API/Data/AppDbContext.cs
+++ REST-API/Data/AppDbContext.cs
@@ -5,16 +5,20 @@ namespace Restful_API.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<FuncionarioCLT> FuncionariosCLT { get; set; }
-        public DbSet<FuncionarioPJ> FuncionariosPJ { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<ContratoCLT> ContratosCLT { get; set; }
+        public DbSet<ContratoPJ> ContratosPJ { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
             => optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<FuncionarioCLT>().HasBaseType(typeof(Funcionario));
-            modelBuilder.Entity<FuncionarioPJ>().HasBaseType(typeof(Funcionario));
+            modelBuilder.Entity<ContratoCLT>()
+                .HasBaseType(typeof(Contrato));
+
+            modelBuilder.Entity<ContratoPJ>()
+                .HasBaseType(typeof(Contrato));
         }
     }
 }
