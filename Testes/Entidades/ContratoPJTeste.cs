2377c4a197c199bbcf68283efd8dd0d05760164a
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-06-30 08:59:27 -0300

    Adicionada verificacao termino e testes

---
+++ Testes/Entidades/ContratoPJTeste.cs
@@ -0,0 +1,16 @@
+﻿using Entidades.Models;
+using Xunit;
+
+namespace Testes.Entidades
+{
+    public class ContratoPJTeste
+    {
+        [Fact]
+        public void Pode_Ser_Instanciado()
+        {
+            var contrato = new ContratoPJ();
+
+            Assert.NotNull(contrato);
+        }
+    }
+}
