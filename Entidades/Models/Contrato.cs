9b4125403b12e613112cde65a250c71d9997e561
Alexis Cesar <42656077+AlexisCesar@users.noreply.github.com>
2022-07-06 09:22:46 -0300

    Aplica template method em salario

--- Entidades/Models/Contrato.cs
+++ Entidades/Models/Contrato.cs
@@ -9,12 +9,12 @@ namespace Entidades.Models
         private DateTime? _termino { get; set; }
         public DateTime? Termino
         {
-            get => _termino; 
+            get => _termino;
             set
             {
-                if(value != null)
+                if (value != null)
                 {
-                    if(value.GetValueOrDefault().Date < Inicio.Date)
+                    if (value.GetValueOrDefault().Date < Inicio.Date)
                     {
                         throw new DomainValidationException("Data de término não pode ser menor do que início.");
                     }
@@ -22,10 +22,18 @@ namespace Entidades.Models
                 _termino = value;
             }
         }
-        public double Salario { get; set; }
+        private double _salario { get; set; }
+        public double Salario { get => calcularSalario(_salario); set { _salario = value; } }
         public string? Cargo { get; set; }
         public Funcionario Funcionario { get; set; } = null!;
 
-        public abstract double calcularSalario();
+        public double calcularSalario(double salarioBruto)
+        {
+            return salarioBruto - calcularDescontosSalario(salarioBruto) + calcularBeneficiosSalario(salarioBruto);
+        }
+
+        public abstract double calcularDescontosSalario(double salarioBruto);
+
+        public abstract double calcularBeneficiosSalario(double salarioBruto);
     }
 }
